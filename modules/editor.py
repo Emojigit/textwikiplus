c3966137d9fd12b6f488c7c83e4d508ca003de2a
Emojigit <yiufamily.hh@gmail.com>
2021-01-11 08:31:19 +0800

    Windows

--- modules/editor.py
+++ modules/editor.py
@@ -1,15 +1,20 @@
-import subprocess, getpass, os
-EDITOR = "/usr/bin/nano"
-ROVIEW = "/usr/bin/less"
-TMPDIR = "/tmp/textwikiplus-"+str(getpass.getuser())+"-"+str(os.getpid())
+import subprocess, getpass, os, tempfile
+EDITOR, ROVIEW = "",""
+if os.name == 'nt':
+    EDITOR = "notepad"
+    ROVIEW = "notepad"
+else:
+    EDITOR = "/usr/bin/nano"
+    ROVIEW = "/usr/bin/less"
+TMPDIR = os.path.join(tempfile.gettempdir(),"textwikiplus-"+str(getpass.getuser())+"-"+str(os.getpid())+".txt")
 
 def editor(init=""):
-    with open(TMPDIR, 'w') as file:
+    with open(TMPDIR, 'w', encoding="utf-8") as file:
         file.write(init)
     subprocess.Popen([EDITOR,TMPDIR]).wait()
-    with open(TMPDIR, 'r') as file:
+    with open(TMPDIR, 'r', encoding="utf-8") as file:
         CONT = file.read()
-    with open(TMPDIR, 'w') as file:
+    with open(TMPDIR, 'w', encoding="utf-8") as file:
         file.write("")
     return CONT
 
@@ -20,8 +25,8 @@ def remove():
     os.remove(TMPDIR)
 
 def ro(content):
-    with open(TMPDIR, 'w') as file:
+    with open(TMPDIR, 'w', encoding="utf-8") as file:
         file.write(content)
     subprocess.Popen([ROVIEW,TMPDIR]).wait()
-    with open(TMPDIR, 'w') as file:
+    with open(TMPDIR, 'w', encoding="utf-8") as file:
         file.write("")
